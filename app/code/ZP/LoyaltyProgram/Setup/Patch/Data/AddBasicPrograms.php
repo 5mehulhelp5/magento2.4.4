85ecbf3556c60b1712a4fe29a8dd9c5b3c4af655
YaroslavKhvostenko <yaroslav.khvostenko@gmail.com>
2024-08-06 12:28:43 +0300

    Code refuctures according to review comments.

--- app/code/ZP/LoyaltyProgram/Setup/Patch/Data/AddBasicPrograms.php
+++ app/code/ZP/LoyaltyProgram/Setup/Patch/Data/AddBasicPrograms.php
@@ -10,6 +10,9 @@ use Magento\Framework\Exception\AlreadyExistsException;
 
 class AddBasicPrograms implements DataPatchInterface
 {
+    public const PROGRAM_MIN = 1;
+    public const PROGRAM_MAX = 2;
+
     public function __construct(
         private LoyaltyProgramInterfaceFactory $loyaltyProgramFactory,
         private LoyaltyProgramResource $loyaltyProgramResource
@@ -24,15 +27,13 @@ class AddBasicPrograms implements DataPatchInterface
         /** @var LoyaltyProgramInterface $programMinimum */
         $programMinimum = $this->loyaltyProgramFactory->create();
         $programMinimum->setProgramName('Program Minimum')
-            ->setDescription('Some Description for Program Minimum')
-            ->setConditionsSerialized('Some Conditions for Program Minimum');
+            ->setDescription('Some Description for Program Minimum');
         $this->loyaltyProgramResource->save($programMinimum);
 
         /** @var LoyaltyProgramInterface $programMaximum */
         $programMaximum = $this->loyaltyProgramFactory->create();
         $programMaximum->setProgramName('Program Maximum')
-            ->setDescription('Some Description for Program Maximum')
-            ->setConditionsSerialized('Some Conditions for Program Maximum');
+            ->setDescription('Some Description for Program Maximum');
         $this->loyaltyProgramResource->save($programMaximum);
 
         return $this;
